286b4385294d78da389149f7a18498714092afa3
vishnu <vishnuprasadkv55@gmail.com>
2019-01-07 02:30:54 +0530

    2 pages

--- js/main.js
+++ js/main.js
@@ -44,7 +44,166 @@ jQuery(document).ready(function () {
     });
 
 });
+// slider
+// // view animate
+AOS.init();
 
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+// view animate
+(function() {
+  
+    var autoUpdate = true,
+        timeTrans = 4000;
+    
+      var cdSlider = document.querySelector('.cd-slider'),
+          item = cdSlider.querySelectorAll("li"),
+          nav = cdSlider.querySelector("nav");
+  
+      item[0].className = "current_slide";
+  
+      for (var i = 0, len = item.length; i < len; i++) {
+          var color = item[i].getAttribute("data-color");
+          item[i].style.backgroundColor=color;
+      }
+  
+      // Detect IE
+      // hide ripple effect on IE9
+      var ua = window.navigator.userAgent;
+          var msie = ua.indexOf("MSIE");
+          if ( msie > 0 ) {
+              var version = parseInt(ua.substring(msie+ 5, ua.indexOf(".", msie)));
+              if (version === 9) { cdSlider.className = "cd-slider ie9";}
+      }
+  
+      if (item.length <= 1) {
+          nav.style.display = "none";
+      }
+  
+      function prevSlide() {
+          var currentSlide = cdSlider.querySelector("li.current_slide"),
+              prevElement = currentSlide.previousElementSibling,
+              prevSlide = ( prevElement !== null) ? prevElement : item[item.length-1],
+              prevColor = prevSlide.getAttribute("data-color"),
+              el = document.createElement('span');
+  
+          currentSlide.className = "";
+          prevSlide.className = "current_slide";
+  
+          nav.children[0].appendChild(el);
+  
+          var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
+              ripple = nav.children[0].querySelector("span");
+  
+          ripple.style.height = size + 'px';
+          ripple.style.width = size + 'px';
+          ripple.style.backgroundColor = prevColor;
+  
+          ripple.addEventListener("webkitTransitionEnd", function() {
+              if (this.parentNode) {
+                  this.parentNode.removeChild(this);
+              }
+          });
+  
+          ripple.addEventListener("transitionend", function() {
+              if (this.parentNode) {
+                  this.parentNode.removeChild(this);
+              }
+          });
+  
+      }
+  
+      function nextSlide() {
+          var currentSlide = cdSlider.querySelector("li.current_slide"),
+              nextElement = currentSlide.nextElementSibling,
+              nextSlide = ( nextElement !== null ) ? nextElement : item[0],
+              nextColor = nextSlide.getAttribute("data-color"),
+              el = document.createElement('span');
+  
+          currentSlide.className = "";
+          nextSlide.className = "current_slide";
+  
+          nav.children[1].appendChild(el);
+  
+          var size = ( cdSlider.clientWidth >= cdSlider.clientHeight ) ? cdSlider.clientWidth*2 : cdSlider.clientHeight*2,
+                ripple = nav.children[1].querySelector("span");
+  
+          ripple.style.height = size + 'px';
+          ripple.style.width = size + 'px';
+          ripple.style.backgroundColor = nextColor;
+  
+          ripple.addEventListener("webkitTransitionEnd", function() {
+              if (this.parentNode) {
+                  this.parentNode.removeChild(this);
+              }
+          });
+  
+          ripple.addEventListener("transitionend", function() {
+              if (this.parentNode) {
+                  this.parentNode.removeChild(this);
+              }
+          });
+  
+      }
+  
+      updateNavColor();
+  
+      function updateNavColor () {
+          var currentSlide = cdSlider.querySelector("li.current_slide");
+  
+          var nextColor = ( currentSlide.nextElementSibling !== null ) ? currentSlide.nextElementSibling.getAttribute("data-color") : item[0].getAttribute("data-color");
+          var	prevColor = ( currentSlide.previousElementSibling !== null ) ? currentSlide.previousElementSibling.getAttribute("data-color") : item[item.length-1].getAttribute("data-color");
+  
+          if (item.length > 2) {
+              nav.querySelector(".prev").style.backgroundColor = prevColor;
+              nav.querySelector(".next").style.backgroundColor = nextColor;
+          }
+      }
+  
+      nav.querySelector(".next").addEventListener('click', function(event) {
+          event.preventDefault();
+          nextSlide();
+          updateNavColor();
+      });
+  
+      nav.querySelector(".prev").addEventListener("click", function(event) {
+          event.preventDefault();
+          prevSlide();
+          updateNavColor();
+      });
+    
+    //autoUpdate
+    setInterval(function() {
+      if (autoUpdate) {
+        nextSlide();
+        updateNavColor();
+      };
+      },timeTrans);
+  
+  })();
+//   slider
 
 jQuery(window).load(function () {
 
